4f2a8a868d17a88df681db5584698dd56639d193
suraj.neupane <suraj.neupane151@gmail.com>
2022-12-14 17:17:18 +0545

    filter added

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -8,20 +8,28 @@ import { context } from '../../prisma/context_new'
 
 const resolvers = {
   Query: {
-    journals: async (parent: unknown, args: {start?:string, end?: string, skip?: number; limit?: number}, context: Context) =>
+    journals: async (parent: unknown, args: { skip?: number; limit?: number }, context: Context) =>
       await context.prisma.journal.findMany({
         take: args.limit,
         skip: args.skip,
-        where:{
+      }),
+    filter: async (parent: unknown, args: { start: string, end: string, skip?: number; limit?: number }, context: Context) =>
+      await context.prisma.journal.findMany({
+        take: args.limit,
+        skip: args.skip,
+        where: {
           date: {
             gte: new Date(args.start),
             lte: new Date(args.end)
           }
         }
       }),
-
     count: async (parent: unknown, args: {}, context: Context) =>
-      await context.prisma.journal.count()
+      await context.prisma.journal.count(),
+    start: async (parent: unknown, args: {}, context: Context) =>
+      await context.prisma.journal.findFirst({ orderBy: [{ date: 'desc' }] }),
+    end: async (parent: unknown, args: {}, context: Context) =>
+      await context.prisma.journal.findFirst({ orderBy: [{ date: 'asc' }] }),
   },
 
   Journal: {
@@ -50,8 +58,11 @@ const resolvers = {
 const typeDefs = `
 scalar Date
 type Query {
-  journals(start: String, end:String, limit:Int, skip: Int): [Journal!]!
+  journals(limit:Int, skip: Int): [Journal!]!
+  filter(start: String!, end:String!, limit:Int, skip: Int): [Journal!]!
   count: Int
+  start: Journal!
+  end: Journal!
 }
 type Journal {
    id: Int
